29234ae3791b5dd3c4719acf44ff7d8b11b3e6d6
paradoxwastaken <63323642+paradoxwastaken@users.noreply.github.com>
2022-02-12 00:31:19 -0500

    Update MainWindow.xaml.cs
    formatting

--- VM_GUI/src/GUI/MainWindow.xaml.cs
+++ VM_GUI/src/GUI/MainWindow.xaml.cs
@@ -23,38 +23,38 @@ namespace ExtensionMethods
 		}
 
 		public int Length()
-        {
+        	{
 			return endOffset - startOffset + 1;
-        }
+        	}
 
 		public bool Empty()
-        {
+        	{
 			return ( startOffset == -1 || endOffset == -1 || str == null );
-        }
+        	}
 
 		public static StringPosition Null()
-        {
+        	{
 			return new( -1, -1, null );
-        }
+        	}
 	}
 
 
 	public static class StringExtensions
 	{
 		public static IntPtr ToPointer( this string str )
-        {
+        	{
 			return Marshal.StringToHGlobalAnsi( str );
 		}
 
 		public static string RemoveIf( this string str, params char[] characters )
-        {
+        	{
 			foreach ( char c in characters )
-            {
+            		{
 				if ( str.Contains( c ) == true )
-                {
+                		{
 					str = str.Replace( c.ToString(), string.Empty );
-                }
-            }
+                		}
+           	 	}
 
 			return str;
 		}
@@ -73,7 +73,7 @@ namespace ExtensionMethods
 		}
 
 		public static string ReplaceAt( this string str, int index, char with )
-        {
+        	{
 			return ( str.ToCharArray()[index] = with ).ToString();
 		}
 
@@ -82,53 +82,53 @@ namespace ExtensionMethods
 			for ( int i = startIndex; i < str.Length; i++ )
 			{
 				if ( str[i] != toFind[0] )
-                {
+                		{
 					continue;
-                }
+                		}
 
 				if ( toFind.Length == 1 )
-                {
+                		{
 					return new( i, i, str[i].ToString() );
-                }
+                		}
 
 				for ( int j = i + 1, k = 1; j < str.Length && k < toFind.Length; j++, k++ )
-                {
+                		{
 					if ( toFind[k] == '*' )
 					{
 						if ( ( k += 1 ) >= toFind.Length )
-                        {
+                        			{
 							return new( i, str.Length, str[i..] );
-                        }
+                        			}
 
 						while ( str[j] != toFind[k] )
-                        {
+                        			{
 							j += 1;
 
 							if ( j >= str.Length )
-                            {
+                            				{
 								return StringPosition.Null();
-                            }
-                        }
+                            				}
+                        			}
 
 						if ( ( k += 1 ) >= toFind.Length )
-                        {
+                        			{
 							return new( i, j, str[i..j] );
-                        }
+                        			}
 
 						if ( ( j += 1 ) >= str.Length )
-                        {
+                        			{
 							return StringPosition.Null();
-                        }
+                        			}
 					}
 
 					if ( str[j] == toFind[k] || toFind[k] == '?' )
-                    {
+                    			{			
 						if ( k + 1 >= toFind.Length )
-                        {
+                        			{
 							return new( i, j, str[i..j] );
-                        }
-                    }
-                }
+                        			}
+                    			}
+                		}
 			}
 
 			return StringPosition.Null();
@@ -150,7 +150,7 @@ namespace ExtensionMethods
 
 namespace VirtualMachine
 {
-    public partial class MainWindow : Window
+    	public partial class MainWindow : Window
 	{
 
 		private string asmTextBoxContents = string.Empty;
@@ -226,36 +226,36 @@ namespace VirtualMachine
 		}
 
 		unsafe public void UpdateStackList()
-        {
+        	{
 			ulong size = 0;
 			long* stack = CPU.GetStack( &size );
 
 			stackList.Items.Clear();
 
 			if ( size == 0 )
-            {
+            		{
 				return;
-            }
+            		}
 
 			for ( int i = ( int )size - 1;; i-- )
 			{
 				stackList.Items.Add( stack[i] );
 				
 				if ( i == 0 )
-                {
+                		{
 					break;
-                }
+                		}
 			}
-        }
+        	}
 
 		private void OnRegisterRightClick( object sender, RoutedEventArgs e )
-        {
+        	{
 			hexDisplay = !hexDisplay;
 			UpdateRegisterList();
-        }
+        	}
 
 		private void OnAssemblerTextChanged( object sender, TextChangedEventArgs e )
-        {
+        	{
 			TextRange textRange = new
 			(
 				asmTextBox.Document.ContentStart,
@@ -265,9 +265,9 @@ namespace VirtualMachine
 			asmTextBoxContents = textRange.Text;
 
 			if ( asmTextBoxContents.EndsWith( '\r' ) == true )
-            {
+            		{
 				return;
-            }
+            		}
 
 			FormattedText asmFormattedText = new
 			( 
@@ -280,12 +280,12 @@ namespace VirtualMachine
 			asmTextBox.HorizontalScrollBarVisibility = ( asmTextBox.Width < asmFormattedText.Width + 12 ) ? ScrollBarVisibility.Visible : ScrollBarVisibility.Hidden;
 		}
 
-        private void OnAssemblerExecute( object sender, RoutedEventArgs e )
-        {
+        	private void OnAssemblerExecute( object sender, RoutedEventArgs e )
+        	{
 			registers = Assembler.Assemble( asmTextBoxContents );
 			UpdateRegisterList();
 			UpdateStackList();
-        }
+        	}
 
 		private void OnFileCreate( object sender, RoutedEventArgs e )
 		{
@@ -294,40 +294,40 @@ namespace VirtualMachine
 			createFileWindow.ShowDialog();
 
 			if ( FileSystem.fileTable.Count != 0 )
-            {
+            		{
 				fileMenu.DataContext = true;
-            }
+            		}
 		}
 
 		unsafe private void OnFileWrite( object sender, RoutedEventArgs e )
 		{
 			if ( fileListBox.SelectedItem != null )
-            {
+            		{
 				if ( FileSystem.TryGetFileTableEntry( fileListBox.SelectedItem.ToString(), out FileSystem.File file ) == true )
-                {
+                		{
 					string text = new TextRange( writeFileText.Document.ContentStart, writeFileText.Document.ContentEnd ).Text;
 					text = text.RemoveIf( '\a', '\b', '\n', '\f', '\t', '\v', '\0' );
 
 					if ( text.Length < ( int )file.size )
-                    {
+                    			{
 						if ( FileSystem.ClearFile( file.name ) == false )
-                        {
+                        			{
 							return;
-                        }
-                    }
+                        			}
+                    			}
 
 					FileSystem.WriteFile( fileListBox.SelectedItem.ToString(), text, ( ulong )text.Length );
 					FileSystem.UpdateFileTable();
 				}
-            }
+            		}		
 		}
 
-        private void OnFileSelect( object sender, RoutedEventArgs e )
-        {
+        	private void OnFileSelect( object sender, RoutedEventArgs e )
+        	{
 			if ( ( sender as ListBox ).SelectedItem == null )
-            {
+            		{
 				return;
-            }
+           	 	}
 
 			selectedFile = ( sender as ListBox ).SelectedItem.ToString();
 			writeFileText.Document.Blocks.Clear();
@@ -344,22 +344,22 @@ namespace VirtualMachine
 			}
 		}
 
-        unsafe private void OnFileDelete( object sender, RoutedEventArgs e )
-        {
+        	unsafe private void OnFileDelete( object sender, RoutedEventArgs e )
+        	{
 			if ( FileSystem.TryGetFileTableEntry( fileListBox.SelectedItem.ToString(), out FileSystem.File file ) == true )
 			{
 				if ( FileSystem.DeleteFile( file.name ) == true )
-                {
+                		{
 					FileSystem.UpdateFileTable();
 					fileListBox.Items.Remove( fileListBox.SelectedItem );
 					writeFileText.Document.Blocks.Clear();
 
 					if ( FileSystem.fileTable.Count == 0 )
-                    {
+                    			{
 						fileMenu.DataContext = true;
 					}
 				}
 			}
-        }
-    }
+        	}
+    	}
 }
